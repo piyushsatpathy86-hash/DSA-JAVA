37e1db6473414177f6dbafac26a70d45e837dd09
piyushsatpathy86-hash <piyushsatpathy86@gmail.com>
2026-07-08 22:35:45 +0530

    Time: 1 ms (86.83%) | Memory: 44.2 MB (87.34%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder sb, int open, int close, int n) {
+        if (sb.length() == 2 * n) {
+            result.add(sb.toString());
+            return;
+        }
+
+        if (open < n) {
+            sb.append('(');
+            backtrack(result, sb, open + 1, close, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        if (close < open) {
+            sb.append(')');
+            backtrack(result, sb, open, close + 1, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
