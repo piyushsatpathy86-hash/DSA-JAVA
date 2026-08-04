32716803705cf278ac8325c1aec7464cb99a7e0a
piyushsatpathy86-hash <piyushsatpathy86@gmail.com>
2026-08-04 21:02:54 +0530

    Time: 3 ms (81.76%) | Memory: 47 MB (42.16%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+            set.add(num);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
